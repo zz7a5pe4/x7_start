63644b813e90e3f1b12de6cf91750b2889180ada
x7fan <x7fan@gmail.com>
2012-03-29 22:56:23 +0800

    add project default page

--- server/sim_s2w.py
+++ server/sim_s2w.py
@@ -10,5 +10,6 @@ s2wDict = { 'X7_Q':'X7_Q_S2W', 'X7_E':'X7_E_S2W', 'X7_RK':'X7_PK_S2W' }
 client = MqClient( s2wDict )
 client.connect()
 client.send( {"type": "log", "mesg":"hello1"} )      
-client.send( {"type": "prog", "mesg":90} )     
+client.send( {"type": "prog", "mesg":90} )  
+client.send( {"type": "cmd", "mesg":"complete"} )     
    
